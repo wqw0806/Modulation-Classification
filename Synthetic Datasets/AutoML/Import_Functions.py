2387ae27ce960a1afde4eaaeae9e4d6ba76a5649
Krishna Srikar Durbha <srikarkrishnad@gmail.com>
2021-04-24 23:49:23 +0530

    Simplifying the Code

---
+++ Synthetic Datasets/AutoML/Import_Functions.py
@@ -0,0 +1,101 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as pimg
+import seaborn as sns
+import scipy.io
+import os
+
+# Importing Data for all SNR Ratio's
+def ImportData(Path):
+	ModulationSchemes = os.listdir(Path)
+
+	Dataset = {}
+	for scheme in ModulationSchemes:
+		DataPath = Path + "/" + scheme
+		files = os.listdir(DataPath)
+		DataofScheme = {}
+		for f in files:
+			Data = scipy.io.loadmat(DataPath + "/" + f)
+			snr = Data['snr'][0][0]
+			rx = Data['rx']
+			InPhase, QuadPhase = rx.real, rx.imag
+			DataofScheme[snr] = np.append(InPhase, QuadPhase, axis=1)
+		Dataset[scheme] = DataofScheme
+
+	return Dataset
+
+
+# Train and Validation Datasets
+"""
+Training:
+Received Signal with SNR Ratio  30 dB is used for Training for both Channels and all Modulation Schemes.
+
+Validation:
+AutoML is validated on Received Signals with SNR Ratio's (in dB)  [−15,−10,−5,0,5,10,15,20,25]
+"""
+def ImportDatasets(Channel,L=None):
+    if Channel == "AWGN":
+        Path =  "../Data/" + Channel
+    elif Channel == "Rayleigh":
+        Path = "../Data/" + Channel + "/" + str(L)
+    Data = ImportData(Path)
+    
+    Dataset = {}
+    Dataset['Classes'] = list(Data.keys())
+    OneHotClasses = np.eye(len(Dataset['Classes']))
+
+    Classes = {}
+    for i in range(len(Dataset['Classes'])):
+        Classes[Dataset['Classes'][i]] = OneHotClasses[i]
+
+    Valid_SNRs = [-15,-10,-5,0,5,10,15,20,25]
+
+    if Channel == "AWGN":
+        X_Train, y_Train = np.empty((0,2)), np.empty((0,3))
+        X_Valid, y_Valid = {}, {}
+
+        for snr in Valid_SNRs:
+            X_Valid[snr] = np.empty((0,2))
+            y_Valid[snr] = np.empty((0,3))
+
+
+        for c in Classes.keys():
+            ModData = Data[c]
+            SNRs = ModData.keys()
+            for snr in SNRs:
+                if snr == 30:
+                    X = ModData[snr]
+                    y = np.repeat(np.expand_dims(Classes[c],axis=0), X.shape[0], axis=0)
+                    X_Train = np.append(X_Train,X,axis=0)
+                    y_Train = np.append(y_Train,y,axis=0)
+                else:
+                    X = ModData[snr]
+                    y = np.repeat(np.expand_dims(Classes[c],axis=0), X.shape[0], axis=0)
+                    X_Valid[snr] = np.append(X_Valid[snr], X, axis=0)
+                    y_Valid[snr] = np.append(y_Valid[snr], y, axis=0)
+
+    elif Channel == "Rayleigh":
+        X_Train, y_Train = np.empty((0,100,2)), np.empty((0,3))
+        X_Valid, y_Valid = {}, {}
+        for snr in Valid_SNRs:
+            X_Valid[snr] = np.empty((0,100,2))
+            y_Valid[snr] = np.empty((0,3))
+
+        for c in Classes.keys():
+            ModData = Data[c]
+            SNRs = ModData.keys()
+            for snr in SNRs:
+                if snr == 30:
+                    X = ModData[snr]
+                    X = X.reshape(-1,100,2)
+                    y = np.repeat(np.expand_dims(Classes[c],axis=0), X.shape[0], axis=0)
+                    X_Train = np.append(X_Train,X,axis=0)
+                    y_Train = np.append(y_Train,y,axis=0)
+                else:
+                    X = ModData[snr]
+                    X = X.reshape(-1,100,2)
+                    y = np.repeat(np.expand_dims(Classes[c],axis=0), X.shape[0], axis=0)
+                    X_Valid[snr] = np.append(X_Valid[snr], X, axis=0)
+                    y_Valid[snr] = np.append(y_Valid[snr], y, axis=0)
+
+    return X_Train, y_Train, X_Valid, y_Valid
---
+++ Synthetic Datasets/AutoML/Plot_Results.py
@@ -0,0 +1,77 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as pimg
+import seaborn as sns
+import scipy.io
+import os
+
+# Evaluating Data
+def EvaluateData(Model, X_Train, y_Train, X_Valid, y_Valid, SavePath, ValidBatchSize=32):
+    Valid_SNR = np.array([-15,-10,-5,0,5,10,15,20,25,30])
+    Accuracy = []
+
+    print ("Evaluating Model")
+    for snr in Valid_SNR:
+        if snr == 30:
+            Loss, Acc = Model.evaluate(X_Train, y_Train, verbose=0)
+        else:
+            Loss, Acc = Model.evaluate(X_Valid[snr], y_Valid[snr], batch_size=ValidBatchSize, verbose=0)
+        print ("SNR:", snr, "Accuracy:", Acc)
+        Accuracy.append(Acc)
+
+    Accuracy = np.array(Accuracy)
+
+    plt.figure(figsize=(10,10))
+    plt.plot(Valid_SNR,Accuracy, color='blue')
+    plt.scatter(Valid_SNR,Accuracy, color='red')
+    plt.title("Accuracy vs SNR")
+    plt.xlabel("SNR")
+    plt.ylabel("Accuracy")
+    plt.grid()
+    plt.savefig(SavePath)
+    plt.show()
+    
+    
+
+# Plot Results
+def PlotResults(Models,  X_Train, y_Train, X_Valid, y_Valid, SavePath, ValidBatchSize=32):
+	Model_Accuracies = []
+	Model_Names = []
+	Valid_SNR = np.array([-15,-10,-5,0,5,10,15,20,25,30])
+	
+	for ModelName,Model in Models.items():
+		Model_Names.append(ModelName)
+		Accuracy = []
+		for snr in Valid_SNR:
+			if snr == 30:
+				try:
+					Loss, Acc = Model.evaluate(X_Train, y_Train, verbose=0)
+				except:
+					X_Train = X_Train.reshape(-1,100,2)
+					y_Train = y_Train[::100]
+					Loss, Acc = Model.evaluate(X_Train, y_Train, verbose=0)
+			else:
+				try:
+					Loss, Acc = Model.evaluate(X_Valid[snr], y_Valid[snr], batch_size=ValidBatchSize, verbose=0)
+				except:
+					X_Valid[snr] = X_Valid[snr].reshape(-1,100,2)
+					y_Valid[snr] = y_Valid[snr][::100]
+					Loss, Acc = Model.evaluate(X_Valid[snr], y_Valid[snr], batch_size=ValidBatchSize, verbose=0)
+				
+			Accuracy.append(Acc)
+
+		Model_Accuracies.append(Accuracy)
+	
+	print ()
+	plt.figure(figsize=(10,10))
+	plt.title("Accuracy for various Models for all SNRs")
+	plt.xlabel("SNR")
+	plt.ylabel("Accuracy")
+	plt.grid()
+	for i in range(len(Model_Accuracies)):
+		Accuracy = Model_Accuracies[i]
+		plt.plot(Valid_SNR,Accuracy, label = Model_Names[i])
+		plt.scatter(Valid_SNR,Accuracy, label = Model_Names[i])
+	plt.legend()
+	plt.savefig(SavePath)
+	plt.show()	
